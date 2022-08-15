0b7cb6392cd0ca4035440e8dcadc1b53392e3a6c
Brian Lockhart <109180146+bslockhart@users.noreply.github.com>
2022-08-14 20:17:48 -0400

    Declared views for .js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
-    
-    
+/* -------------------- BEGINS DECLARING GLOBAL CONSTANTS AND VARIABLES -------------------- */
+/* ---------- declares the different views/screens to turn them on and off ---------- */
+const startQuizScreenEl = document.getElementById("start-quiz-screen");
+const quizScreenEl = document.getElementById("quiz-screen");
+const resultScreenEl = document.getElementById("result-screen");
+const allDoneScreenEl = document.getElementById("all-done-screen");
+const highScoresScreenEl = document.getElementById("high-scores-screen");
