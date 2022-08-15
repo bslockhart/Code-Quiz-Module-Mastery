e4e5150932ad7c370f05fb6eb92899833de93bb6
Brian Lockhart <109180146+bslockhart@users.noreply.github.com>
2022-08-14 20:18:50 -0400

    Updated js. to show HTML elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,18 @@ const quizScreenEl = document.getElementById("quiz-screen");
 const resultScreenEl = document.getElementById("result-screen");
 const allDoneScreenEl = document.getElementById("all-done-screen");
 const highScoresScreenEl = document.getElementById("high-scores-screen");
+
+/* ---------- declares constants to point to the different html elements and buttons needed for this project ---------- */
+const headerEl = document.getElementById("header");
+const viewHighScoresEl = document.getElementById("view-high-scores");
+const timerEl = document.getElementById("timer");
+const startBtn = document.getElementById("start-quiz-btn");
+const goBackBtn = document.getElementById("go-back-btn");
+const clearHighScoresBtn = document.getElementById("clear-high-scores-btn");
+const questionEl = document.getElementById("question");
+const answersListEl = document.getElementById("answers-list");
+const resultEl = document.getElementById("result");
+const finalScoreEl = document.getElementById("final-score");
+const highScoreFormEl = document.getElementById("high-score-form");
+const initialsEl = document.getElementById("initials");
+const highScoresListEl = document.getElementById("high-scores-list");
