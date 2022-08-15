713a3d2601418268a01ecb7f66a52f1f69f128ca
Brian Lockhart <109180146+bslockhart@users.noreply.github.com>
2022-08-14 20:19:44 -0400

    Created array declarations .js

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,34 @@ const highScoresListEl = document.getElementById("high-scores-list");
 
 // declares timer for quiz
 const quizTime = 75;
+
+// declares the array of multiple choice questions (objects): q: questions, a: answers and c: correct answers
+const mcq = [
+    {
+        q: "Commonly used data types DO Not Include:",
+        a: ["strings", "booleans", "alerts", "numbers"],
+        c: "alerts",
+    },
+    {
+        q: "The condition in an if / else statement is enclosed with ________.",
+        a: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        c: "parenthesis",
+    },
+    {
+        q: "Arrays in JavaScript can be used to store _______.",
+        a: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        c: "all of the above",
+    },
+    {
+        q:
+            "String values must be enclosed within _______ when being assigned to variables.",
+        a: ["commas", "curly brackets", "quotes", "parenthesis"],
+        c: "quotes",
+    },
+    {
+        q:
+            "A very useful tool used during development and debugging for printing content to the debugger is:",
+        a: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        c: "console.log",
+    },
+];
